d4ad801bb79cf11e4872045055f98760edf962cc
Evgeniy Karpov <random783@gmail.com>
2026-02-17 16:14:17 +0300

    initial commit

---
+++ src/test/java/SelenideAllureListenerClass.java
@@ -0,0 +1,29 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideAllureListenerClass {
+
+
+    @Test
+    public void issueSearchTest(){
+        SelenideLogger.addListener("allure",new AllureSelenide());
+
+        open("https://github.com");
+        $(".header-search-button").click();
+        $("#query-builder-test").sendKeys("haid26/HW7");
+        $("#query-builder-test").submit();
+
+        $("[href=\"/Haid26/HW7\"]").click();
+        $("#issues-tab").click();
+        $(withText("New TestIssue #126")).should(Condition.exist);
+
+    }
+}
